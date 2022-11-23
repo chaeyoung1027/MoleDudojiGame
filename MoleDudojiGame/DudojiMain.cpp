1a170acc7db7c5d535dc30ff7739c72bb6dfb81f
chaeyoung1027 <s2111@e-mirim.hs.kr>
2022-11-23 10:20:20 +0900

    이미지 추가

--- MoleDudojiGame/DudojiMain.cpp
+++ MoleDudojiGame/DudojiMain.cpp
@@ -2,10 +2,8 @@
 #include<SFML/Graphics.hpp>
 #include<iostream>
 
-#define WIDTH 544
-#define HEIGHT 340
-
-#define TOY_HAMMER   100.f
+#define WIDTH 1100
+#define HEIGHT 942
 
 using namespace sf;
 using namespace std;
@@ -13,24 +11,21 @@ using namespace std;
 int main(void) {
     int x = 0, y = 0;
 
-    //마우스 이동시 따라 움직일 Circle 객체
-    CircleShape circle_shape(TOY_HAMMER);
-    circle_shape.setFillColor(Color::Green);
-
-    circle_shape.setRadius(TOY_HAMMER);
-    circle_shape.setOutlineColor(sf::Color::Red);
-    //circle_shape.setOutlineThickness(0); 겉 테두리
-    circle_shape.setPosition(x, y);
-
-
 	RenderWindow app(VideoMode(WIDTH, HEIGHT), "Catch Dudoji");
     app.setFramerateLimit(60);
     
-    Texture t1;
-    t1.loadFromFile("images/background.jpg");
+    Texture background, hammer;
+    background.loadFromFile("images/background.png");
+    hammer.loadFromFile("images/뿅망치.png");
+    /*t1.loadFromFile("images/두더지1.png");
+    t1.loadFromFile("images/두더지2.png");
+    t1.loadFromFile("images/두더지3.png");
+    t1.loadFromFile("images/두더지4.png");
+    t1.loadFromFile("images/두더지5.png");
+    t1.loadFromFile("images/두더지6.png");*/
 
     Sprite s1;
-    s1.setTexture(t1);
+    s1.setTexture(background);
 
     //x누르면 종료되는 코드
     //SFML 메인 루프 - 윈도우가 닫힐때 까지 반복
@@ -45,26 +40,28 @@ int main(void) {
                 app.close();
                 cout << "프로그램이 종료되었습니다." << endl;
             }
+            switch (event.type) {
+            case Event::MouseButtonPressed:
+            case Mouse::Left:
+            {
 
-            //마우스 버튼 뗌(Released)
-                switch (event.type)
-                {
-                case Event:: MouseMoved:
-                {
-                    Vector2i pos = Mouse::getPosition(app);
-                    cout << "이동 : pos.x = " << pos.x << " pos.y = " << pos.y << endl;
-                    x = pos.x;
-                    y = pos.y;
-                    break;
+            }
+            }
+
+            switch (event.type)
+            {
+            case Event:: MouseMoved:
+            {
+                Vector2i pos = Mouse::getPosition(app);
+                cout << "이동 : pos.x = " << pos.x << " pos.y = " << pos.y << endl;
+                x = pos.x;
+                y = pos.y;
+                break;
                 }
             }
         }
         app.clear();
         app.draw(s1);
-        
-        //Circloe 위치 보정
-        circle_shape.setPosition(Vector2f(x - 100.f, y - 100.f));
-        app.draw(circle_shape);
 
         //프레임을 스크린에 출력
         app.display();
